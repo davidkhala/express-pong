cc7fdc694e294bef30baf1235dc2e054994877fd
davidLiu <david-khala@hotmail.com>
2019-03-21 13:45:15 +0800

    order of errorCode is sensitive

--- app.js
+++ app.js
@@ -9,3 +9,19 @@ app.post('/post', (req, res) => {
 	console.info(req.url, 'req.body', req.body);
 	res.json(req.body);
 });
+
+app.get('/err', (req, res) => {
+	throw Error(req.url);
+});
+
+//error Handle middleware is order sensitive
+app.use((err, req, res, next) => {
+	console.error(err);
+	if (res.headersSent) {
+		console.log('res.headersSent');
+		return next(err);
+	}
+	res.status(err.status ? err.status : 500);
+	res.send(err);
+});
+
