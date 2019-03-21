31a423af041da19b8f82342229ae0a5b874106dd
davidLiu <david-khala@hotmail.com>
2019-03-21 11:12:42 +0800

    add simple post

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const {run} = require('khala-nodeutils').baseApp();
 const port = process.env.port ? process.env.port : 80;
 const {app} = run(port);
 app.get('/', (req, res) => {
-	console.info('get ping');
+	console.info(req.url);
 	res.send('pong from server');
+});
+app.post('/post', (req, res) => {
+	console.info(req.url, 'req.body', req.body);
+	res.json(req.body);
 });
