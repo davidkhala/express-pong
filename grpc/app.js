08f1a41a6fc1328e9464baaa6225139782c8538e
david <david-khala@hotmail.com>
2020-02-14 17:06:04 +0800

    enrich travis test coverage grpcServer use 9090 instead of 2020

--- grpc/app.js
+++ grpc/app.js
@@ -2,7 +2,7 @@ const {grpcServer, load} = require('khala-grpc');
 const path = require('path');
 const pingProtoPath = path.resolve(__dirname, 'ping.proto');
 const {PingService: {service}} = load(pingProtoPath).object;
-const port = process.env.port ? process.env.port : 2020;
+const port = process.env.port ? process.env.port : 9090;
 
 
 const baseUrl = `localhost:${port}`;
--- grpc/test/ping.js
+++ grpc/test/ping.js
@@ -2,7 +2,9 @@ const {grpcRequest} = require('khala-grpc');
 const path = require('path');
 const protoPath = path.resolve(__dirname, '../ping.proto');
 
-const url = 'localhost:2020';
+const {host} = process.env.host || 'localhost';
+const {port} = process.env.port || 9090;
+const url = `${host}:${port}`;
 
 const ping = async () => {
 	const result = await grpcRequest(protoPath, 'PingService', url, 'ping', {});
