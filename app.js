a29f4135c0b1e4e68dd097147fff09e3f44fc079
DavidLiu <david.yx.liu@oracle.com>
2021-04-13 10:51:54 -0400

    add node help

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 #!/usr/bin/env node
+if (process.argv[2] === 'help') {
+    console.info(`
+        environments:
+            [port]      : default to 80
+            [extensions]: post|err|formData, any or any combinations of these options, concat by ','  
+        Example:
+            extensions=post,err,formData port=3000 node app.js
+    `)
+    process.exit(0)
+}
+
 const {run} = require('khala-nodeutils/baseApp');
 const port = process.env.port || 80;
+
 const {app} = run(port);
 app.get('/', (req, res) => {
     console.info(req.url);
--- https/app.js
+++ https/app.js
@@ -1,9 +1,26 @@
 #!/usr/bin/env node
+if (process.argv[2] === 'help') {
+    console.info(`
+        environments:
+            [port]  : default to 443
+            key     : Required. TLS Key file path
+            cert    : Required. TLS Cert file path
+        Example:
+            key=[key file path] cert=[cert file path] node app.js
+    `)
+    process.exit(0)
+}
 const {run} = require('khala-nodeutils/baseApp');
 const port = process.env.port || 443;
-
 const {key, cert} = process.env
-const tlsOptions = {key, cert, ca: cert};
+
+if (!key) {
+    throw Error('Missing TLS Key file')
+}
+if (!cert) {
+    throw Error('Missing TLS Cert file')
+}
+const tlsOptions = {key, cert};
 if (process.version.match(/^(v10\.|v12\.|v14\.)/)) {
     const minVersion = 'TLSv1.2'
     Object.assign(tlsOptions, {minVersion})
