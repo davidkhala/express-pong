ae30ad4bbb6a672ff201e1f0773d45e6cdf627bb
David Liu <david.yx.liu@oracle.com>
2021-07-30 06:01:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const port = process.env.port || 80;
 
 const {app} = run(port);
 app.get('/', (req, res) => {
-    console.info(req.url);
+    console.info(`url:${req.url}; from ${req.ip}`);
     res.send('pong\n');
 });
 require('./extensions')(app, process.env.extensions)
