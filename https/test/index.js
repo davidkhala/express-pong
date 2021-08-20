32afdb2d248f1c465e2e016e5d40669a72f2d5cf
DavidLiu <david.yx.liu@oracle.com>
2021-08-20 11:50:06 +0800

    debug pm2
    Signed-off-by: DavidLiu <david.yx.liu@oracle.com>

--- https/test/index.js
+++ https/test/index.js
@@ -1,43 +1,48 @@
-const {execSync} = require('child_process')
-const assert = require('assert')
-const {sleep} = require('khala-light-util')
+const assert = require('assert');
 const {axiosPromise} = require('khala-axios');
 const path = require('path');
 const cert = path.resolve(__dirname, 'cert.pem');
 const key = path.resolve(__dirname, 'key.pem');
 const port = 3443;
+const {PM2} = require('khala-pm2');
+const {sleep} = require('khala-light-util');
 const httpsPing = async () => {
-    const url = `https://localhost:${port}`;
+	const url = `https://localhost:${port}`;
 
-    const result = await axiosPromise({url, method: 'GET'}, {
-        ca: cert,
-    });
-    assert.strictEqual(result, 'pong\n');
+	const result = await axiosPromise({url, method: 'GET'}, {
+		ca: cert,
+	});
+	assert.strictEqual(result, 'pong\n');
 };
 describe('https', function () {
-    this.timeout(3000)
-    before(async () => {
-        execSync('pm2 --version')
+	this.timeout(0);
+	const pm2 = new PM2();
+	const name = 'https';
+	before(async () => {
 
-        try {
-            execSync('pm2 delete https')
-        } catch (e) {
-            console.error(e)
-        }
-        const app = path.resolve(__dirname, '../app.js')
-        execSync(`key=${key} cert=${cert} port=${port} pm2 start ${app} --name https`, {encoding: 'utf-8'})
-        await sleep(1000)
-    })
+		await pm2.connect();
+		const app = path.resolve(__dirname, '../app.js');
+		const env = {
+			key,
+			cert,
+			port,
+		};
 
-    it('https ping', async () => {
-        await httpsPing();
-    })
-    it('pm2 logs', () => {
-        let result = execSync('pm2 status', {encoding: 'utf-8'});
-        console.log(result)
-        result = execSync('pm2 logs https --nostream --lines 10', {encoding: 'utf-8'})
-        console.log(result)
-    })
+		const existingApp = await pm2.get(name);
+		if (!existingApp) {
+			await pm2.run({name, script: app, env});
+			await sleep(1000);// TODO This is must for waiting until ready
+		}
 
+	});
 
-})
+	it('https ping', async () => {
+		await httpsPing();
+	});
+
+	after(async () => {
+		// await pm2.delete(name);
+		pm2.disconnect();
+	});
+
+});
--- test/index.js
+++ test/index.js
@@ -1,23 +1,23 @@
-const {execSync} = require('child_process')
-const {axiosPromise} = require('khala-axios')
-const {sleep} = require('khala-light-util')
-const assert = require('assert')
+const {execSync} = require('child_process');
+const {axiosPromise} = require('khala-axios');
+const {sleep} = require('khala-light-util');
+const assert = require('assert');
 describe('http', function () {
-    this.timeout(3000)
-    const port = 3000
-    before(async () => {
-        execSync('pm2 --version')
-        try {
-            execSync('pm2 delete http')
-        } catch (e) {
-            console.error(e)
-        }
-        execSync(`port=${port} pm2 start app.js --name http`, {encoding: 'utf-8'})
-        await sleep(1000)
-    })
-    it('ping', async () => {
-        const url = `http://localhost:${port}/`
-        const pongResult = await axiosPromise({url, method: 'GET'})
-        assert.strictEqual(pongResult, 'pong\n')
-    })
-})
+	this.timeout(3000);
+	const port = 3000;
+	before(async () => {
+		execSync('pm2 --version');
+		try {
+			execSync('pm2 delete http');
+		} catch (e) {
+			console.error(e);
+		}
+		execSync(`port=${port} pm2 start app.js --name http`, {encoding: 'utf-8'});
+		await sleep(1000);
+	});
+	it('ping', async () => {
+		const url = `http://localhost:${port}/`;
+		const pongResult = await axiosPromise({url, method: 'GET'});
+		assert.strictEqual(pongResult, 'pong\n');
+	});
+});
