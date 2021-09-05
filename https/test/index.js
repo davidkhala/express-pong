6cabe00e1daf47005cda03fc2e52c47999955226
DavidLiu <david.yx.liu@oracle.com>
2021-09-05 21:25:49 +0800

    debug node

--- https/test/index.js
+++ https/test/index.js
@@ -4,19 +4,15 @@ const path = require('path');
 const cert = path.resolve(__dirname, 'cert.pem');
 const port = 3443;
 const domain = process.env.fqdn || 'localhost';
-const httpsPing = async () => {
-	const url = `https://${domain}:${port}`;
-
-	const result = await axiosPromise({url, method: 'GET'}, {
-		ca: cert, rejectUnauthorized: false,
-	});
-	assert.strictEqual(result, '\npong\n');
-};
 describe('https', () => {
 
-
-	it('ping', async function () {
+	it('ping express-pongs', async function () {
 		this.timeout(0);
-		await httpsPing();
+		const url = `https://${domain}:${port}`;
+
+		const result = await axiosPromise({url, method: 'GET'}, {
+			ca: cert, rejectUnauthorized: false,
+		});
+		assert.strictEqual(result, '\npong\n');
 	});
 });
