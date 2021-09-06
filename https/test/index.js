c6a02429551e72a6a9d9f85a5db8e54e10bdbf82
DavidLiu <david.yx.liu@oracle.com>
2021-09-06 08:26:15 +0800

    ip rotate

--- https/test/index.js
+++ https/test/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cert = path.resolve(__dirname, 'cert.pem');
 const port = process.env.port || 3443;
 const domain = process.env.fqdn || 'localhost';
+const ca = path.resolve(__dirname, 'ca.cer');
 describe('https', () => {
 
 	it('ping express-pongs', async function () {
@@ -11,7 +12,7 @@ describe('https', () => {
 		const url = `https://${domain}:${port}`;
 
 		const result = await axiosPromise({url, method: 'GET'}, {
-			cert,
+			cert,ca
 		});
 		assert.strictEqual(result, '\npong\n');
 	});
